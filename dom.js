c87d8429f70d4b314316be60abee84bc5ef659f2
Avani Arora <avani.arora@uber.com>
2020-08-20 15:52:51 +0530

    final changes made to load about and contact page inplace

--- dom.js
+++ dom.js
@@ -14,5 +14,40 @@ function toggleText(elementid) {
     paragraph.getElementsByClassName("hiddentext")[0].style.display = "none";
 
   }
+
   //console.log(paragraph.getElementsByClassName("hiddentext"));
 }
+function goToAbout()
+{
+
+  document.getElementById("blogs").style.display = "none";
+  document.getElementById("aboutinfo").style.display = "block";
+  document.getElementById("contactinfo").style.display = "none";
+
+  document.getElementById("showcasehome").style.display = "none";
+  document.getElementById("showcaseabout").style.display = "block";
+  document.getElementById("showcasecontact").style.display = "none";
+
+
+}
+function goToHome()
+{
+  document.getElementById("blogs").style.display = "block";
+  document.getElementById("aboutinfo").style.display = "none";
+  document.getElementById("contactinfo").style.display = "none";
+  document.getElementById("showcasehome").style.display = "block";
+  document.getElementById("showcaseabout").style.display = "none";
+  document.getElementById("showcasecontact").style.display = "none";
+
+}
+function goToContact()
+{
+  document.getElementById("blogs").style.display = "none";
+  document.getElementById("aboutinfo").style.display = "none";
+  document.getElementById("contactinfo").style.display ="block";
+  document.getElementById("showcasehome").style.display = "none";
+  document.getElementById("showcaseabout").style.display = "none";
+  document.getElementById("showcasecontact").style.display = "block";
+
+}
+
